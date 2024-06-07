475d9e133cbbc53a0530a4f2c2c7e53a8923aeaf
01akshat09 <134699525+01akshat09@users.noreply.github.com>
2024-06-07 22:13:07 +0530

    Sidebar completed

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,33 @@
+import { Search } from "lucide-react";
+import React from "react";
+
+const HomePage = () => {
+  const handdleSearchRecipie = () => {
+    console.log("Hey");
+  };
+  return (
+    <div className="bg-[#faf9fb] p-10 flex-1">
+      <div className="max-w-screen-lg mx-auto">
+        <form onSubmit={handdleSearchRecipie}>
+          <label className="input flex items-center gap-2 shadow-md">
+            <Search size={"24"} />
+            <input
+              type="text"
+              placeholder="What do you want to cook today?"
+              className="text-sm md:text-md grow"
+            />
+          </label>
+        </form>
+
+        <h1 className="font-bold text-3xl md:text-5xl mt-4">
+          Recommend Recipes
+        </h1>
+        <p className="font-semibold text-slate-500  ml-1 my-2 text-sm tracking-tight">
+          Popular Choices
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
