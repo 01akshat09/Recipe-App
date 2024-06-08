5e5c23e8e1a09e49355ce1ec882df293fa0eb9f1
01akshat09 <134699525+01akshat09@users.noreply.github.com>
2024-06-08 17:26:46 +0530

    Completed

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,33 +1,91 @@
 import { Heart, HeartPulse, Soup } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
-const RecipeCard = () => {
+const getTwoValuesFromArray = (arr) => {
+  return [arr[0], arr[1]];
+};
+
+const RecipeCard = ({ recipe, bg, badge }) => {
+  const healthLabels = getTwoValuesFromArray(recipe.healthLabels);
+  const [isFavorite, setIsFavorite] = useState(
+    localStorage.getItem("favorites")?.includes(recipe.label)
+  );
+  const addRecipeToFavorites = () => {
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    const isRecipeAlreadyFavorite = favorites.some(
+      (fav) => fav.label === recipe.label
+    );
+
+    if (isRecipeAlreadyFavorite) {
+      favorites = favorites.filter((fav) => fav.label !== recipe.label);
+      setIsFavorite(false);
+    } else {
+      favorites.push(recipe);
+      setIsFavorite(true);
+    }
+
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  };
   return (
-    <div className="flex flex-col rounded-md overflow-hidden p-3 relative">
-      <a href="" target="_blank" className="relative h-32">
-        {/* <div className="skeleton absolute inset-0" /> */}
+    <div
+      className={`flex flex-col ${bg} rounded-md overflow-hidden p-3 relative`}
+    >
+      <a
+        href={`https://www.youtube.com/results?search_query=${recipe.label}`}
+        target="_blank"
+        className="relative h-32"
+      >
+        <div className="skeleton absolute inset-0" />
         <img
-          src="/1.jpg"
-          alt="food"
-          className="rounded-md h-full w-full object-cover cursor-pointer 
-        "
+          src={recipe.image}
+          alt="recipe image"
+          className="rounded-md h-full w-full object-cover cursor-pointer opacity-0 transition-opacity duration-500"
+          onLoad={(e) => {
+            e.currentTarget.style.opacity = 1;
+            e.currentTarget.previousElementSibling.style.display = "none";
+          }}
         />
         <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
-          <Soup size={16} /> 2 Servings
+          <Soup size={16} /> {recipe.yield} Servings
         </div>
-        <div className="absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer">
-          <Heart size={20} className="hover:fill-red-500 hover:text-red-500" />
+        <div
+          className="absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer"
+          onClick={(e) => {
+            e.preventDefault();
+            addRecipeToFavorites();
+          }}
+        >
+          {!isFavorite && (
+            <Heart
+              size={20}
+              className="hover:fill-red-500 hover:text-red-500"
+            />
+          )}
+          {isFavorite && (
+            <Heart size={20} className="fill-red-500 text-red-500" />
+          )}
         </div>
       </a>
       <div className="flex mt-1">
-        <p className="font-bold tracking-wide">Good Maggie</p>
+        <p className="font-bold tracking-wide">{recipe.label}</p>
       </div>
-      <p className="my-2">DOlly Kitchen</p>
+      <p className="my-2">
+        {recipe.cuisineType[0].charAt(0).toUpperCase() +
+          recipe.cuisineType[0].slice(1)}{" "}
+        Kitchen
+      </p>
       <div className="flex gap-2 mt-auto">
-        <div className="flex gap-1 items-center p-1 rounded-md">
-          <HeartPulse size={16} />
-          <span className="text-sm tracking-tighter font-semibold">Hey</span>
-        </div>
+        {healthLabels.map((label, index) => (
+          <div
+            key={index}
+            className={`flex gap-1 items-center ${badge} p-1 rounded-md`}
+          >
+            <HeartPulse size={16} />
+            <span className="text-sm tracking-tighter font-semibold">
+              {label}
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
